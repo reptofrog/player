28e5fe4d902d645b3ee349016cb491901c246469
Zappedfish <zappedfish@gmail.com>
2022-11-28 23:12:40 +0400

    Player time reporting

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import YouTube from 'react-youtube';
 
 const Player = (state: any): JSX.Element => {
@@ -6,6 +6,7 @@ const Player = (state: any): JSX.Element => {
 
     const player: any = useRef();
     const loadedTrackId: any = useRef(-1);
+    const [effectDependencyTrigger, setEffectDependencyTrigger] = useState();
 
     const opts = {
         height: '0',
@@ -16,7 +17,7 @@ const Player = (state: any): JSX.Element => {
             disablekb: 1,
             fs: 0,
             modestbranding: 1,
-            rel: 0,
+            rel: 0
             /*start end*/
         },
     };
@@ -36,8 +37,18 @@ const Player = (state: any): JSX.Element => {
     const playerOnReady = (e: any) => {
         player.current = e;
         loadedTrackId.current = state.get.currentTrackID;
+        setEffectDependencyTrigger(!effectDependencyTrigger as any); // Triggering useEffect by changing a value
 
         const p = player.current.target;
+
+        // This is needed so that playback is started even if user pressed the 'play' button before the player is loaded
+        if(state.get.isCurrentTrackPlaying) {
+            if(state.get.currentTrackID == loadedTrackId.current) {
+                p.playVideo();
+            } else {
+                loadedTrackId.current = state.get.currentTrackID;
+            }
+        }
     }
 
     useEffect(() => {
@@ -51,7 +62,6 @@ const Player = (state: any): JSX.Element => {
                     } else {
                         loadedTrackId.current = state.get.currentTrackID;
                     }
-                    
                 } else {
                     p.pauseVideo();
                 }
@@ -62,13 +72,36 @@ const Player = (state: any): JSX.Element => {
 
     useEffect(() => {
         function fun(ev: any) {
-            console.log('a');
+            // Check that the event was sent from the YouTube iFrame
+            if(ev.source === iframeWindow) {
+              var data = JSON.parse(ev.data);
+        
+              // The "infoDelivery" event is used by YT to transmit any
+              // kind of information change in the player,
+              // such as the current time or a playback quality change.
+              if(
+                data.event === "infoDelivery" &&
+                data.info &&
+                data.info.currentTime
+              ) {
+                let time = data.info.currentTime;
+        
+                console.log(time);
+              }
+            }
         } 
 
-        window.addEventListener('message', fun)
+        if(player.current && player.current.target.getIframe()) {
+            // This is the source "window" that will emit the events.
+            var iframeWindow = player.current.target.getIframe().contentWindow;
 
-        return window.removeEventListener('message', fun)
-    }, [state.get.currentTrackID])
+            window.addEventListener('message', fun)
+        }
+
+        return () => {
+            window.removeEventListener('message', fun)
+        }
+    }, [effectDependencyTrigger])
 
     return(
         <YouTube
